74985b52341e2b44570b41fb2a808b0a3188d272
Maximilian Schmidt <m.schmidt@obis-concept.de>
2019-01-16 12:25:04 +0100

    Initial Commit

---
+++ Classes/Controller/StandardController.php
@@ -0,0 +1,23 @@
+<?php
+namespace ObisConcept\ShortUrls\Controller;
+
+/*
+ * This file is part of the ObisConcept.ShortUrls package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class StandardController extends ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ObisConcept.ShortUrls
+A short URL module for the Neos CMS.
